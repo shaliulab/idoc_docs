524e084147c08c6f5455d21d9d67d78d78876bdf
antortjim <ntoniohu@gmail.com>
2022-01-13 15:10:18 +0100

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'IDOC'
+copyright = '2022, Sha Liu Lab'
+author = 'Sha Liu'
 
 release = '0.1'
 version = '0.1.0'
